89df6c84a3ebfb3b9b8f5c06d89f2e02b0950961
Phong Nguyen <phongnguyen887@csu.fullerton.edu>
2024-10-22 16:39:52 -0700

    Update database.js
    Reference the DATABASE_URL

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -2,9 +2,11 @@
 import postgres from 'postgres'
 
 // see env variables in .env
-import {PGCONNECT} from '$env/static/private';
+//import {PGCONNECT} from '$env/static/private';
 
-const sql = postgres(PGCONNECT, {} )
+import {DATABASE_URL} from '$env/static/private';
+
+const sql = postgres(DATABASE_URL, {} )
 
 export default sql;
 
